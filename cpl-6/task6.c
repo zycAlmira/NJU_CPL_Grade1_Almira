f6f6802cbb8d5ed7fc3a12772f76f1d0112087e1
Almira <617603924@qq.com>
2024-11-03 11:50:46 +0800

    task1 ac task2 ac task3 ac task4 ac task5 ac task6 ac

--- cpl-6/task6.c
+++ cpl-6/task6.c
@@ -1,35 +1,33 @@
 #include <stdio.h>
+#define ma 2000000000
 int main(void){
-    int n,m,t,d,min=1000000000,mid,b=1;
+    int n,m,t,result=ma,max,min,mid;
     int N[1000005],M[1000005];
     scanf("%d%d%d",&n,&m,&t);
-    for(int i=1;i<=n;++i){
+    for(int i=1;i<=n;i++){
         scanf("%d",&N[i]);
     }
     for(int i=0;i<m;++i){
         scanf("%d",&M[i]);
     }
-    for(int i=0;i<m;i++){
-        int mid=n/2;
-        while(b) {
-            if ((t - N[mid] - M[i]) < 0) {
-                mid = (mid + 1) / 2;
-                continue;
-            } else if ((t - N[mid] - M[i]) == 0) {
-                b=0;
-            } else if ((t - N[mid] - M[i]) > 0) {
-                if((t - N[mid] - M[i])<min){
-
-                }
+    for(int i=0;i<m;++i){
+        max=n;
+        min=1;
+        mid=(max+min)/2;
+        while(min<=max){
+            if(t-N[mid]-M[i]>=0){
+                result=(result<(t-N[mid]-M[i]))?result:(t-N[mid]-M[i]);
+                min=mid+1;
+                mid=(max+min)/2;
+            }else{
+                max=mid-1;
+                mid=(max+min)/2;
             }
-            if
         }
     }
-    if(min==2000000000){
-        printf("-1");
+    if(result==ma){
+        result=-1;
     }
-    else{
-        printf("%d",min);
-    }
+    printf("%d",result);
     return 0;
 }
