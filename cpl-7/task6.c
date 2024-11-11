8e3458155da990e2297f2722512c29e16aefe7f7
Almira <617603924@qq.com>
2024-11-11 15:27:44 +0800

    cpl-7 task1 unknown mistake 100 task2 unknown mistake 92 task3 ac task4 ac task5 90 效率和内存 task6 ac

--- cpl-7/task6.c
+++ cpl-7/task6.c
@@ -3,22 +3,24 @@
 //
 #include <stdio.h>
 #include <string.h>
-
+#include <limits.h>
 int main(void) {
     int n, m, d, board[400][400] = {0}, value[400][400] = {0}, max1[1000] = {0}, max2[1000] = {
-            0}, max = -2000000000, num = 0;
+            0}, max = INT_MIN, num = 0;
     scanf("%d%d%d", &n, &m, &d);
     for (int i = d; i < n + d; i++) {
         for (int j = d; j < m + d; j++) {
             scanf("%d", &board[i][j]);
         }
     }
-    for (int i = 1; i <= d; ++i) {
+    value[d][d] = board[d][d];
+    for (int i = 1; i <= d; i++) {
         for (int j = 1; j <= i; j++) {
-            value[d][d] += board[d + j][d + i - j] + board[d - j][d + i - j] + board[d + i - j][d + j] +
-                           board[d - i + j][d - j];
+            value[d][d] += board[d + j][d + i - j] + board[d - j][d - i + j] + board[d - i + j][d + j] +
+                           board[d + i - j][d - j] ;
         }
     }
+    printf("%d",value[d][d]);
     for (int h = 1; h < n; h++) {
         value[d + h][d] = value[d + h - 1][d] + board[d + h + d][d] - board[d + h - 1 - d][d];
         for (int i = 1; i <= d; ++i) {
@@ -31,7 +33,7 @@ int main(void) {
         for (int j = d + 1; j < m + d; j++) {
             value[i][j] = value[i][j - 1] + board[i][j + d] - board[i][j - 1 - d];
             for (int h = 1; h <= d; h++) {
-                value[i][j] += board[i + h][j + d - h] + board[i - h][j + d - h] - board[i + h][j - d + h - 1] +
+                value[i][j] += board[i + h][j + d - h] + board[i - h][j + d - h] - board[i + h][j - d + h - 1] -
                                board[i - h][j - d + h - 1];
             }
         }
@@ -44,12 +46,12 @@ int main(void) {
                 memset(max1, 0, sizeof(max1));
                 memset(max2, 0, sizeof(max2));
                 num++;
-                max1[num] = i;
-                max2[num] = j;
+                max1[num] = i-d+1;
+                max2[num] = j-d+1;
             } else if (max == value[i][j]) {
                 num++;
-                max1[num] = i;
-                max2[num] = j;
+                max1[num] = i-d+1;
+                max2[num] = j-d+1;
             }
         }
     }
